f173f3cc17ab7cb65ff242d76fd6c31bd3bca8cf
quangthanh72 <130241493+quangthanh72@users.noreply.github.com>
2023-07-12 17:09:52 +0700

    updatedata

---
+++ controllers/admin.js
@@ -0,0 +1,56 @@
+const Product = require('../models/products')
+
+exports.getAddProduct = (req, res, next) => {
+    res.render("admin/edit-product", {
+      pageTitle: "Add product",
+      path: "/admin/add-product",
+      editing: false
+    });
+  }
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title  
+  const imageUrl = req.body.imageUrl
+  const price = req.body.price
+  const description = req.body.description
+  const product = new Product(title,imageUrl,price,description)
+    product.save()
+    res.redirect("/");
+  }
+
+
+  exports.getEditProduct = (req, res, next) => {
+    const editMode  = req.query.edit
+    if(!editMode){
+      return res.redirect('/')
+    }
+    const proId = req.params.productId
+    Product.findByiId(proId, product =>{
+      if(!product){
+        return res.redirect('/')
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product
+      });
+    })
+    
+  }
+
+exports.postEditProduct = (req,res,next)=>{
+  
+}
+
+    
+exports.getProducts = (req,res,next) => {
+  Product.fetchAll((products)=>{
+    res.render("admin/products", {
+      prods: products,
+      docTitle: "Shop",
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  })
+}
